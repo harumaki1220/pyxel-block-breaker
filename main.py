c3214f9f426ff0b8bd6156f1c8cf694dbf5cbde5
harumaki1220 <haruma181220@gmail.com>
2025-11-03 03:22:29 +0900

    feat: Add paddle movement

--- main.py
+++ main.py
@@ -3,27 +3,36 @@ import pyxel
 SCREEN_WIDTH = 160
 SCREEN_HEIGHT = 120
 
+PADDLE_WIDTH = 40
+PADDLE_HEIGHT = 5
+PADDLE_SPEED = 2
+
 class App:
     def __init__(self):
         pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="ブロック崩し")
-        pyxel.mouse(True)
-        self.number = 0
+        
+        self.paddle_x = (SCREEN_WIDTH - PADDLE_WIDTH) // 2
+        self.paddle_y = SCREEN_HEIGHT - PADDLE_HEIGHT - 5
+
         pyxel.run(self.update, self.draw)
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_ESCAPE):
             pyxel.quit()
         
-        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
-            if 20 <= pyxel.mouse_x <= 40 and 50 <= pyxel.mouse_y <= 70:
-                self.number -= 1
-            elif 100 <= pyxel.mouse_x <= 120 and 50 <= pyxel.mouse_y <= 70:
-                self.number += 1
+        if pyxel.btnp(pyxel.KEY_LEFT):
+            self.paddle_x = self.paddle_x - PADDLE_SPEED
+        if pyxel.btnp(pyxel.KEY_RIGHT):
+            self.paddle_x = self.paddle_x + PADDLE_SPEED
 
     def draw(self):
-        pyxel.cls(pyxel.COLOR_DARK_BLUE)
-        pyxel.text(SCREEN_WIDTH // 2 - 10, SCREEN_HEIGHT // 2, f"{self.number}", pyxel.COLOR_YELLOW)
-        pyxel.text(30, 60, "-", pyxel.COLOR_WHITE)
-        pyxel.text(110, 60, "+", pyxel.COLOR_WHITE)
+        pyxel.cls(pyxel.COLOR_BLACK)
+        pyxel.rect(
+            self.paddle_x,
+            self.paddle_y,
+            PADDLE_WIDTH,
+            PADDLE_HEIGHT,
+            pyxel.COLOR_WHITE
+        )
 
 App()
