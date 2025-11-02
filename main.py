033b6d3d144d79e6826aacfbc8457e99bb39ac06
harumaki1220 <haruma181220@gmail.com>
2025-11-03 05:04:50 +0900

    feat: Add paddle bounds checking

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ PADDLE_WIDTH = 40
 PADDLE_HEIGHT = 5
 PADDLE_SPEED = 2
 
+BALL_RADIUS = 2
+
 class App:
     def __init__(self):
         pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="ブロック崩し")
@@ -14,16 +16,42 @@ class App:
         self.paddle_x = (SCREEN_WIDTH - PADDLE_WIDTH) // 2
         self.paddle_y = SCREEN_HEIGHT - PADDLE_HEIGHT - 5
 
+        self.ball_x = self.paddle_x
+        self.ball_y = self.paddle_y - 5
+
+        self.ball_vx = 1
+        self.ball_vy = -1
+
         pyxel.run(self.update, self.draw)
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_ESCAPE):
             pyxel.quit()
         
-        if pyxel.btnp(pyxel.KEY_LEFT):
+        if pyxel.btn(pyxel.KEY_LEFT):
             self.paddle_x = self.paddle_x - PADDLE_SPEED
-        if pyxel.btnp(pyxel.KEY_RIGHT):
+        if pyxel.btn(pyxel.KEY_RIGHT):
             self.paddle_x = self.paddle_x + PADDLE_SPEED
+
+        if self.paddle_x < 0:
+            self.paddle_x = 0
+        if self.paddle_x > SCREEN_WIDTH - PADDLE_WIDTH:
+            self.paddle_x = SCREEN_WIDTH - PADDLE_WIDTH
+
+        self.ball_x += self.ball_vx
+        self.ball_y += self.ball_vy
+
+        # 当たり判定のロジック
+        if self.ball_x <= BALL_RADIUS or self.ball_x >= SCREEN_WIDTH - BALL_RADIUS:
+            self.ball_vx = -self.ball_vx
+        if self.ball_y <= BALL_RADIUS or self.ball_y >= SCREEN_HEIGHT - BALL_RADIUS:
+            self.ball_vy = -self.ball_vy
+
+        # パドルとの当たり判定
+        if self.paddle_x <= self.ball_x and self.ball_x <= self.paddle_x + PADDLE_WIDTH:
+            if self.ball_y + BALL_RADIUS >= self.paddle_y:
+                if self.ball_vy > 0:
+                    self.ball_vy = -self.ball_vy
 
     def draw(self):
         pyxel.cls(pyxel.COLOR_BLACK)
@@ -32,6 +60,12 @@ class App:
             self.paddle_y,
             PADDLE_WIDTH,
             PADDLE_HEIGHT,
+            pyxel.COLOR_WHITE
+        )
+        pyxel.circ(
+            self.ball_x,
+            self.ball_y,
+            BALL_RADIUS,
             pyxel.COLOR_WHITE
         )
 
