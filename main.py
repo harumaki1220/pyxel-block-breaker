b727a93731a639ac527826fd24a3c916ecfc7872
harumaki1220 <haruma181220@gmail.com>
2025-11-03 21:38:29 +0900

    feat: Implement game over

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class App:
                 block = {"x": col * BLOCK_WIDTH, "y": row * BLOCK_HEIGHT + 10, "alive": True}
                 self.blocks.append(block)
 
+        self.scene = "play"
         pyxel.run(self.update, self.draw)
 
     def reset_ball(self):
@@ -38,65 +39,71 @@ class App:
     def update(self):
         if pyxel.btnp(pyxel.KEY_ESCAPE):
             pyxel.quit()
-        
-        if pyxel.btn(pyxel.KEY_LEFT):
-            self.paddle_x = self.paddle_x - PADDLE_SPEED
-        if pyxel.btn(pyxel.KEY_RIGHT):
-            self.paddle_x = self.paddle_x + PADDLE_SPEED
 
-        if self.paddle_x < 0:
-            self.paddle_x = 0
-        if self.paddle_x > SCREEN_WIDTH - PADDLE_WIDTH:
-            self.paddle_x = SCREEN_WIDTH - PADDLE_WIDTH
+        if self.scene == "play":
+            if pyxel.btn(pyxel.KEY_LEFT):
+                self.paddle_x = self.paddle_x - PADDLE_SPEED
+            if pyxel.btn(pyxel.KEY_RIGHT):
+                self.paddle_x = self.paddle_x + PADDLE_SPEED
 
-        self.ball_x += self.ball_vx
-        self.ball_y += self.ball_vy
+            if self.paddle_x < 0:
+                self.paddle_x = 0
+            if self.paddle_x > SCREEN_WIDTH - PADDLE_WIDTH:
+                self.paddle_x = SCREEN_WIDTH - PADDLE_WIDTH
 
-        # 当たり判定のロジック
-        if self.ball_x <= BALL_RADIUS or self.ball_x >= SCREEN_WIDTH - BALL_RADIUS:
-            self.ball_vx = -self.ball_vx
-        if self.ball_y <= BALL_RADIUS:
-            self.ball_vy = -self.ball_vy
-        if self.ball_y >= SCREEN_HEIGHT - BALL_RADIUS:
-            self.reset_ball()
+            self.ball_x += self.ball_vx
+            self.ball_y += self.ball_vy
 
-        # パドルとの当たり判定
-        if self.paddle_x <= self.ball_x and self.ball_x <= self.paddle_x + PADDLE_WIDTH:
-            if self.ball_y + BALL_RADIUS >= self.paddle_y:
-                if self.ball_vy > 0:
-                    self.ball_vy = -self.ball_vy
+            # 当たり判定のロジック
+            if self.ball_x <= BALL_RADIUS or self.ball_x >= SCREEN_WIDTH - BALL_RADIUS:
+                self.ball_vx = -self.ball_vx
+            if self.ball_y <= BALL_RADIUS:
+                self.ball_vy = -self.ball_vy
+            if self.ball_y >= SCREEN_HEIGHT - BALL_RADIUS:
+                self.scene = "gameover"
 
-        for block in self.blocks:
-            if block["alive"]:
-                if block["x"] <= self.ball_x and self.ball_x <= block["x"] + BLOCK_WIDTH:
-                    if block["y"] <= self.ball_y and self.ball_y <= block["y"] + BLOCK_HEIGHT:
-                        block["alive"] = False
+            # パドルとの当たり判定
+            if self.paddle_x <= self.ball_x and self.ball_x <= self.paddle_x + PADDLE_WIDTH:
+                if self.ball_y + BALL_RADIUS >= self.paddle_y:
+                    if self.ball_vy > 0:
                         self.ball_vy = -self.ball_vy
-                        break
+
+            for block in self.blocks:
+                if block["alive"]:
+                    if block["x"] <= self.ball_x and self.ball_x <= block["x"] + BLOCK_WIDTH:
+                        if block["y"] <= self.ball_y and self.ball_y <= block["y"] + BLOCK_HEIGHT:
+                            block["alive"] = False
+                            self.ball_vy = -self.ball_vy
+                            break
 
     def draw(self):
         pyxel.cls(pyxel.COLOR_BLACK)
-        pyxel.rect(
-            self.paddle_x,
-            self.paddle_y,
-            PADDLE_WIDTH,
-            PADDLE_HEIGHT,
-            pyxel.COLOR_WHITE
-        )
-        pyxel.circ(
-            self.ball_x,
-            self.ball_y,
-            BALL_RADIUS,
-            pyxel.COLOR_WHITE
-        )
-        for block in self.blocks:
-            if block["alive"]:
-                pyxel.rect(
-                    block["x"],
-                    block["y"],
-                    BLOCK_WIDTH - 1,
-                    BLOCK_HEIGHT - 1,
-                    pyxel.COLOR_LIGHT_BLUE
-                )
+
+        if self.scene == "play":
+            pyxel.rect(
+                self.paddle_x,
+                self.paddle_y,
+                PADDLE_WIDTH,
+                PADDLE_HEIGHT,
+                pyxel.COLOR_WHITE
+            )
+            pyxel.circ(
+                self.ball_x,
+                self.ball_y,
+                BALL_RADIUS,
+                pyxel.COLOR_WHITE
+            )
+            for block in self.blocks:
+                if block["alive"]:
+                    pyxel.rect(
+                        block["x"],
+                        block["y"],
+                        BLOCK_WIDTH - 1,
+                        BLOCK_HEIGHT - 1,
+                        pyxel.COLOR_LIGHT_BLUE
+                    )
+
+        elif self.scene == "gameover":
+            pyxel.text(60, 60, "GAME OVER", pyxel.COLOR_WHITE)
 
 App()
